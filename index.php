8e98dc358c6f6c4ae7373a6e8bb32a1e0c586374
James Turner <me@jamesturner.im>
2015-08-01 12:02:53 +0100

    Remove recent posts from homepage

--- index.php
+++ index.php
@@ -9,18 +9,7 @@
 </div>
 
 <section>
-    <h1>Recent Posts</h1>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    <article class="recent-post">
-        <h2><?php the_title(); ?></h2>
-        <p><?php the_excerpt(); ?></p>
-        <footer><?php the_date(); ?></footer>
-    </article>
-    <?php endwhile; else: ?>
 
-        <p>Sorry, no posts to list</p>
-
-    <?php endif; ?>
 </section>
 
 <?php get_footer(); ?>
