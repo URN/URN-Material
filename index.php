d7ba5df426c2f4687ecd701611a66315b5e266f5
James Turner <me@jamesturner.im>
2015-08-16 17:32:02 +0100

    Remove now playing module

--- index.php
+++ index.php
@@ -1,15 +1,3 @@
 <?php get_header(); ?>
 
-<div id="now-playing">
-    <div id="now-playing-bar">
-        <div id="now-playing-progress" style="width:65%"></div>
-        <span>Now Playing: <strong>Ed Sheeran - Don't</strong></span>
-    </div>
-    <a href="#">Listen Now!</a>
-</div>
-
-<section>
-
-</section>
-
 <?php get_footer(); ?>
