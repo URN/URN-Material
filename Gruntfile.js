3e9c013a1679afdae10e48ff2d638593f2b290db
James Turner <me@jamesturner.im>
2015-08-16 17:06:05 +0100

    Add watch (live reload) and compass/sass compile tasks to Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,36 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-    });
+        pkg: grunt.file.readJSON('package.json'),
 
-    grunt.initConfig({
         scsslint: {
             all: [
                 'sass/**/*.scss'
             ]
+        },
+
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: true,
+            },
+            css: {
+                files: ['sass/**/*.scss'],
+                tasks: ['compass'],
+            },
         }
     });
 
     grunt.loadNpmTasks('grunt-scss-lint');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('default', ['scsslint']);
-
+    grunt.registerTask('default', ['scsslint', 'compass', 'watch']);
 };
