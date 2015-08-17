848130ff9cff448b27315812bddb920ab4a435f4
James Turner <me@jamesturner.im>
2015-08-17 22:14:15 +0100

    Add basic homepage structure and styling

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php get_header(); ?>
 
+<div class="row">
+    <div class="module">?</div>
+    <div class="module">Slideshow</div>
+    <div class="module">Recent Shows/Events</div>
+</div>
+
+<div class="row">
+    <div class="twitter">
+        <a class="twitter-timeline"  href="https://twitter.com/urn1350" data-widget-id="633379687427895296" width="360">Tweets by @urn1350</a>
+        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+    </div>
+    <div class="row vertical">
+        <div class="clip">
+            <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292950-students-with-disabilities-special-molly-o-brien-statement/embed/v3?eid=AQAAAApF0lUWPzIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
+        </div>
+        <div class="clip">
+            <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292768-students-complain-about-grad-ball/embed/v3?eid=AQAAAG9F0lVgPjIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
+        </div>
+        <div class="schedule">Schedule</div>
+    </div>
+</div>
+
+<div class="row">
+    <div class="module">URN TV Watch Now!</div>
+    <div class="module">
+        <h1>Recent Coverage</h1>
+        <ul>
+            <li class="recent-post">Women's World Cup</li>
+            <li class="recent-post">Varsity Cricket</li>
+            <li class="recent-post">Goodbyes and others</li>
+        </ul>
+    </div>
+    <div class="module">Catch up with shows you've missed (podcasts)</div>
+</div>
+
 <?php get_footer(); ?>
