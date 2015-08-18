baaef12c5297075eb6179bbc90f6dd7577d3847b
Harry Mumford-Turner <harrymt@me.com>
2015-08-18 12:50:26 +0100

    Added default page template
    This will be the default template for displaying pages.
    
    When creating a page in Wordpress, on the RHS under ‘Template’, the
    dropdown item ‘Default Template’ will reflect this page.php as a
    template.

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ */
+get_header(); ?>
+
+<h1>Post Content</h1>
+<?php the_content(); ?>
+</h1>End Post content</h1>
+
+<?php get_footer(); ?>
