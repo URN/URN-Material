ec2eecd66fd0464e2ea7f47037f13e466765855f
James Turner <me@jamesturner.im>
2015-08-20 18:06:14 +0100

    Add command to mirror grunt task to update site url after import

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,7 +32,8 @@ module.exports = function(grunt) {
                 command: [
                     'ssh urn \'mysqldump -u readonlyuser wordpress > /tmp/wordpress-dump.sql\'',
                     'scp urn:/tmp/wordpress-dump.sql /tmp/wordpress-dump.sql',
-                    'mysql -u root -ppassword wordpress < /tmp/wordpress-dump.sql'
+                    'mysql -u root -ppassword wordpress < /tmp/wordpress-dump.sql',
+                    'mysql -u root -ppassword -e "UPDATE wordpress.wp_options SET option_value = \'${URNLOCAL}\' WHERE option_name = \'siteurl\' OR option_name= \'home\';"'
                 ].join('&&')
             },
             options: {
