f8fa5849b6818183cd414b66e7ea24bf4368b4d2
Harry Mumford-Turner <harrymt@me.com>
2015-08-29 15:22:48 +0100

    Display basic show data for individual shows
    Display basic show information mainly as a test

--- taxonomy-shows.php
+++ taxonomy-shows.php
@@ -1,9 +1,46 @@
 <?php
+/*
+ * Displays content for a certain show
+ *
+ */
+get_header(); ?>
 
-get_header();
+<div class="row">
+    <div class="content">
 
-echo "A show";
+        <h1><?php single_cat_title(); ?></h1>
+        <h2>Description</h2>
 
-get_footer();
+        <?php if (term_description()) : ?>
+            <p><?php echo term_description(); ?></p>
+        <?php else : ?>
+            <p>No description set</p>
+        <?php endif; ?>
 
-?>
+
+        <h2>Posts</h2>
+        <?php if (have_posts()) : ?>
+
+            <?php while (have_posts()) : the_post(); ?>
+
+                <?php
+                    the_ID();
+                    the_content();
+                    the_permalink();
+                    the_title();
+                    the_category();
+                    get_the_time('Y-m-j');
+                ?>
+
+            <?php endwhile; ?>
+
+        <?php else : ?>
+
+            <p><?php _e( 'No posts found for ' . single_cat_title('', false) . '.'); ?></p>
+
+        <?php endif; ?>
+
+    </div>
+</div>
+
+<?php get_footer(); ?>
