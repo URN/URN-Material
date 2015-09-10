472922522d3f7102270751db6d79d991f5f0015f
Harry Mumford-Turner <harrymt@me.com>
2015-09-10 01:16:14 +0100

    Stopped editor-style.scss from being linted

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,8 +5,7 @@ module.exports = function(grunt) {
 
         scsslint: {
             all: [
-                'sass/_partials/*.scss',
-                'sass/*.scss'
+                'sass/_partials/*.scss'
             ]
         },
 
