61305dfc52d625f641501080409c092e75a23419
James Turner <me@jamesturner.im>
2015-09-12 15:56:11 +0100

    When a link 404s, check whether it exists on the old site and redirect if so

--- 404.php
+++ 404.php
@@ -1,1 +1,14 @@
+<?php
+$requestPath = $_SERVER['REQUEST_URI'];
+
+$oldLink = 'http://old.urn1350.net' . $requestPath;
+
+$file_headers = @get_headers($oldLink);
+
+if ($file_headers[0] !== 'HTTP/1.1 404 Not Found') {
+    header('Location: ' . $oldLink);
+    die();
+}
+?>
+
 404
