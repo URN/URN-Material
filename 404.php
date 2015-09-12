f15637537595c7eac04b79e35e6b821919afb5d2
James Turner <me@jamesturner.im>
2015-09-12 17:52:47 +0100

    Add content to 404 page

--- 404.php
+++ 404.php
@@ -9,6 +9,12 @@ if ($file_headers[0] !== 'HTTP/1.1 404 Not Found') {
     header('Location: ' . $oldLink);
     die();
 }
-?>
 
-404
+get_header();
+?>
+<div class="main-content">
+    <h1>Page not found!</h1>
+    <p>Sorry we couldn't find the page you were looking for on this site (or the old one!)</p>
+    <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
+</div>
+<?php get_footer(); ?>
