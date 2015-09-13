d4c92edbff132a845d00cc260fbe8fbceff76901
Harry Mumford-Turner <harrymt@me.com>
2015-09-13 22:58:46 +0100

    Add page for single blog posts

---
+++ single.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The template for displaying single blog posts.
+ *
+ */
+get_header(); ?>
+
+<div class="main-content">
+    <div class="entry-content">
+
+        <?php the_title( '<h1>', '</h1>' ); ?>
+
+        <p><?php echo get_the_date(); ?></p>
+
+    <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+
+            // Include the page content template.
+            the_content();
+
+        // End the loop.
+        endwhile;
+        ?>
+    </div>
+</div>
+<?php get_footer(); ?>
