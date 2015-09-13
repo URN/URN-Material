7a19b95350d5fc822247b1182b62d493e54ba2a9
James Turner <me@jamesturner.im>
2015-09-13 13:02:44 +0100

    Place homepage content inside a .main-content container div

--- index.php
+++ index.php
@@ -1,80 +1,81 @@
 <?php get_header(); ?>
 
-<div class="row">
-    <div class="module">?</div>
-    <div class="module">Slideshow</div>
-    <div class="module">Recent Shows/Events</div>
-</div>
-
-<div class="row">
-    <div class="twitter">
-        <a class="twitter-timeline"  href="https://twitter.com/urn1350" data-widget-id="633379687427895296" width="360">Tweets by @urn1350</a>
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<div class="main-content">
+    <div class="row">
+        <div class="module">?</div>
+        <div class="module">Slideshow</div>
+        <div class="module">Recent Shows/Events</div>
     </div>
-    <div class="row vertical">
-        <h1>Headlines</h1>
-        <div class="clip">
-            <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292950-students-with-disabilities-special-molly-o-brien-statement/embed/v3?eid=AQAAAApF0lUWPzIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
+
+    <div class="row">
+        <div class="twitter">
+            <a class="twitter-timeline"  href="https://twitter.com/urn1350" data-widget-id="633379687427895296" width="360">Tweets by @urn1350</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
         </div>
-        <div class="clip">
-            <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292768-students-complain-about-grad-ball/embed/v3?eid=AQAAAG9F0lVgPjIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
-        </div>
-        <h1>Schedule</h1>
-        <div class="schedule mini-schedule">
-            <ul class="day-names">
-                <li class="day-name">
-                    Today
-                    <a href="/schedule" class="btn">View Full Schedule</a>
-                </li>
-            </ul>
-            <div class="timetable">
-                <ul class="times">
-                    <li>7am</li>
-                    <li>8am</li>
-                    <li>9am</li>
-                    <li>10am</li>
-                    <li>11am</li>
-                    <li>Noon</li>
-                    <li>1pm</li>
-                    <li>2pm</li>
-                    <li>3pm</li>
-                    <li>4pm</li>
-                    <li>5pm</li>
-                    <li>6pm</li>
-                    <li>7pm</li>
-                    <li>8pm</li>
-                    <li>9pm</li>
-                    <li>10pm</li>
-                    <li>11pm</li>
-                    <li>Midnight</li>
-                    <li>1am</li>
-                    <li>2am</li>
-                    <li>3am</li>
-                    <li>4am</li>
-                    <li>5am</li>
-                    <li>6am</li>
-                </ul>
-                <ul class="days">
-                    <li class="day">
-                        <ul class="slots"></ul>
+        <div class="row vertical">
+            <h1>Headlines</h1>
+            <div class="clip">
+                <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292950-students-with-disabilities-special-molly-o-brien-statement/embed/v3?eid=AQAAAApF0lUWPzIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
+            </div>
+            <div class="clip">
+                <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292768-students-complain-about-grad-ball/embed/v3?eid=AQAAAG9F0lVgPjIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
+            </div>
+            <h1>Schedule</h1>
+            <div class="schedule mini-schedule">
+                <ul class="day-names">
+                    <li class="day-name">
+                        Today
+                        <a href="/schedule" class="btn">View Full Schedule</a>
                     </li>
                 </ul>
+                <div class="timetable">
+                    <ul class="times">
+                        <li>7am</li>
+                        <li>8am</li>
+                        <li>9am</li>
+                        <li>10am</li>
+                        <li>11am</li>
+                        <li>Noon</li>
+                        <li>1pm</li>
+                        <li>2pm</li>
+                        <li>3pm</li>
+                        <li>4pm</li>
+                        <li>5pm</li>
+                        <li>6pm</li>
+                        <li>7pm</li>
+                        <li>8pm</li>
+                        <li>9pm</li>
+                        <li>10pm</li>
+                        <li>11pm</li>
+                        <li>Midnight</li>
+                        <li>1am</li>
+                        <li>2am</li>
+                        <li>3am</li>
+                        <li>4am</li>
+                        <li>5am</li>
+                        <li>6am</li>
+                    </ul>
+                    <ul class="days">
+                        <li class="day">
+                            <ul class="slots"></ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-<div class="row">
-    <div class="module">URN TV Watch Now!</div>
-    <div class="module">
-        <h1>Recent Coverage</h1>
-        <ul>
-            <li class="recent-post">Women's World Cup</li>
-            <li class="recent-post">Varsity Cricket</li>
-            <li class="recent-post">Goodbyes and others</li>
-        </ul>
+    <div class="row">
+        <div class="module">URN TV Watch Now!</div>
+        <div class="module">
+            <h1>Recent Coverage</h1>
+            <ul>
+                <li class="recent-post">Women's World Cup</li>
+                <li class="recent-post">Varsity Cricket</li>
+                <li class="recent-post">Goodbyes and others</li>
+            </ul>
+        </div>
+        <div class="module">Catch up with shows you've missed (podcasts)</div>
     </div>
-    <div class="module">Catch up with shows you've missed (podcasts)</div>
 </div>
-
 <?php get_footer(); ?>
