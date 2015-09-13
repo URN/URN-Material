e2db2ae70da6c9e38325b0fc19a01ae89a9df2a3
Harry Mumford-Turner <harrymt@me.com>
2015-09-13 19:52:03 +0100

    Added progressive loading to audiobook feed
    Tidied up Audioboom feed JS.
    Set audioboom feeds to display 3 on load, to reduce browser load.
    Moved Audioboom feed to Flexbox

--- js/audio-boom-feed.js
+++ js/audio-boom-feed.js
@@ -1,41 +1,80 @@
 
+var audioboom_feeds = [];
+
 jQuery(document).ready(function( $ ) {
     'use strict';
 
     var audioboom_feed = {
 
+        // Setup elements & load config
         init: function(element) {
+
+            // Setup base and load more button
+            var feed = this;
+            var $load_more = $('<button>', { class: 'btn load-more' }).text('Load more').click(function() {
+                feed.loadCachedPodcasts();
+            });
+            $(element).append(
+                $('<ul>', { class: 'cards'}).append($('<li>'))
+            ).append($load_more);
+
             this.config = {
                 api_url: '//api.audioboom.com/' + $(element).attr('data-channel-audioboom-type') +
                     '/' + $(element).attr('data-channel-id') + '/audio_clips',
                 feed_name: $(element).attr('data-channel-name'),
                 feed_type: $(element).attr('data-channel-type'),
-                element: element
+                element: $(element).find('.cards'),
+                more_button: $load_more,
+                max_display: 2, // index based
+                current_shown: 0 // index based
             };
 
-            this.getData(this.processPodcasts);
+            this.loadPodcasts();
         },
 
-        processPodcasts: function(feed, podcasts) {
-            var $podcasts = $('<ul>', { class: 'cards'});
+        // Load podcasts from memory rather than making an api call
+        loadCachedPodcasts: function() {
+            this.displayPodcasts(this, this.cached_podcasts);
+        },
 
-            for (var i = 0; i < podcasts.length; i++) {
-                $podcasts.append(feed.makeCard(feed, podcasts[i]));
+        // Load podcasts via api call
+        loadPodcasts: function() {
+            this.getData(this.displayPodcasts);
+        },
+
+        displayPodcasts: function(feed, podcasts) {
+            for (var i = feed.config.current_shown; i < podcasts.length; i++) {
+                console.log('i ' + i + ', curr_shown: ' + feed.config.current_shown + ', podlength: ' + podcasts.length);
+
+                if (podcasts.length - 1 == feed.config.current_shown) {
+                    // Reached max podcasts
+                    console.log('Reached max podcasts');
+                    // Set button to null
+                    feed.config.more_button.text('No more').attr('disabled', 'true');
+                    break;
+                }
+
+                if (i > feed.config.max_display + feed.config.current_shown) {
+                    feed.config.current_shown = i;
+                    console.log('Ending, curr_shown ' + i);
+                    break;
+                }
+
+                $(feed.config.element).append(feed.makeCard(feed, podcasts[i]));
             }
-            $(feed.config.element).append($podcasts);
         },
 
         makeEmbedUrl: function(url) {
-            var embed_config = 'player_theme=light&' +
-                               'link_color=%235e44cb&' + // %23 should be #
-                               'image_option=none&' +
+            var embed_config = 'player_theme=light&amp;' +
+                               'link_color=%235e44cb&amp;' +
+                               'image_option=none&amp;' +
                                'show_title=true';
-            return encodeURIComponent(url.replace('https://', '//embeds.') + '/embed/v3?' + embed_config);
+            return url.replace('https://', '//embeds.') + '/embed/v3?' + embed_config;
         },
 
         makeCard: function(feed, podcast) {
             var $podcast_image = $('<img>', {src: podcast.urls.image});
-            var $show_type_button = $('<button>', {class: 'btn ' + feed.config.feed_type}).text(feed.config.feed_name);
+            var $show_type_button = $('<button>', { class: 'btn ' + feed.config.feed_type}).text(feed.config.feed_name);
             var $podcast_embed = $('<iframe>', { scrolling: 'no', src: feed.makeEmbedUrl(podcast.urls.detail)}).css('width', '100%');
             var $podcast_description = $('<p>').text(podcast.description);
 
@@ -49,19 +88,24 @@ jQuery(document).ready(function( $ ) {
             );
         },
 
+        // Get data from Audio Boom
+        // e.g. https://api.audioboom.com/channels/3139695/audio_clips
         getData: function(callback) {
             var feed = this;
-            // e.g. https://api.audioboom.com/channels/3139695/audio_clips
             $.ajax({
                 url: this.config.api_url,
                 type: 'get'
             }).done(function (data) {
+                feed.cached_podcasts = data.body.audio_clips;
                 callback(feed, data.body.audio_clips);
             });
         }
     };
 
+    // Initialise each audioboom element & add them to global array for debugging & more
     $('.audioboom-feed').each(function() {
         audioboom_feed.init($(this));
+        audioboom_feeds.push(audioboom_feed);
     });
 });
+
