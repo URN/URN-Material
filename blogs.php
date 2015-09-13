5cc7363d79a50f856a9551492a3fdb0922d9ede8
Harry Mumford-Turner <harrymt@me.com>
2015-09-13 22:58:24 +0100

    Restyled blogs so it gets the all the last blog posts, not separated by shows

--- blogs.php
+++ blogs.php
@@ -16,60 +16,28 @@ get_header(); ?>
 
                 // Include the page content template.
                 the_content();
-
-            // End the loop.
-            endwhile;
-        ?>
-    </div>
+            ?>
+    </div><!-- /.entry-content -->
 
     <div class="blog-wrapper">
         <?php
-            $shows = get_terms( 'shows', 'orderby=count&hide_empty=0' );
-            $count = count($shows);
-            if ( $count > 0 ) {
-                foreach ( $shows as $show ) {
-                    if ($show->count > 0) {
-                        echo "<h1 class='show-title'>";
-                        echo "<a href=" . get_permalink( get_page_by_path( $show->slug ) ) . "'>";
-                        echo $show->name;
-                        echo "</a>";
-                        echo "</h1>";
+            // Get all the blogs
+            $posts = get_posts(array(
+              'post_type' => 'post',
+              'numberposts' => 100,
+              'orderby' => 'date'
+            ));
 
-                        // Get all the blog posts on that show
-                        $posts = get_posts(array(
-                          'post_type' => 'post',
-                          'numberposts' => -1,
-                          'tax_query' => array(
-                            array(
-                              'taxonomy' => 'shows',
-                              'field' => 'id',
-                              'terms' => $show->term_id,
-                              'include_children' => true
-                            )
-                          )
-                        ));
-                        echo "<ul class='blog-excerpt'>";
-                        foreach ( $posts as $post ) {
-
-                            echo "<li>";
-                                echo "<h2>";
-                                    echo "<a href='" . get_permalink($post->ID) . "'>";
-                                    echo $post->post_title;
-                                    echo "</a>";
-                                echo "</h2>";
-                                echo "<p>" . get_the_date('', $post->ID) . "</p>";
-                                echo "<a href='" . get_permalink($post->ID) . "'>";
-                                echo "    <button class='btn btn-default'>View post</button>";
-                                echo "</a>";
-                                // echo "<pre>";
-                                // print_r($post);
-                                // echo "</pre>";
-                            echo "</li>";
-                        }
-                        echo "</ul>";
-                    }
-                }
+            echo "<ul class='blog-excerpt'>";
+            foreach ( $posts as $post ) {
+                setup_postdata( $post );
+                echo format_blog_excerpt($post);
             }
+            wp_reset_postdata();
+            echo "</ul>";
+
+        // End the loop.
+        endwhile;
         ?>
     </div>
 </div> <!-- /.main-content -->
