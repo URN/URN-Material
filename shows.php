368388a52573d3a2fdbf5780a963130d132ed9d3
Harry Mumford-Turner <harrymt@me.com>
2015-09-14 17:58:53 +0100

    Added base shows page

--- shows.php
+++ shows.php
@@ -1,12 +1,30 @@
 <?php
 /**
-* Template Name: Shows
-*/
+ * Template Name: Shows
+ */
+get_header(); ?>
 
-get_header();
+<div class="main-content">
+    <div class="entry-content">
+    <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
 
-echo "All shows";
+            // Include the page content template.
+            the_content();
 
-get_footer();
+        // End the loop.
+        endwhile;
+        ?>
+    </div>
 
-?>
+    <h1>Page is a Work in process</h1>
+
+    <h1 id="afterdark">After Dark</h1>
+    <h1 id="culture">Culture</h1>
+    <h1 id="daytime">Daytime</h1>
+    <h1 id="news">News</h1>
+    <h1 id="sport">Sports</h1>
+</div>
+
+<?php get_footer(); ?>
