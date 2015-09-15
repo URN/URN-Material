34e9df9a24246d282fda50b264b46d73a729d8ea
Harry Mumford-Turner <harrymt@me.com>
2015-09-15 15:13:29 +0100

    Renamed your news tab headings to fix #48

--- your-news.php
+++ your-news.php
@@ -37,14 +37,14 @@ get_header(); ?>
             </div>
 
             <input id="tab-four" type="radio" name="grp" />
-            <label for="tab-four" class="tab-title">Varsity</label>
+            <label for="tab-four" class="tab-title">Uni of Sport</label>
             <div class="tab-content">
                  <!-- Blog posts with author Sports show -->
                 <?php echo get_posts_by_author(10, 41); ?>
             </div>
 
             <input id="tab-five" type="radio" name="grp" />
-            <label for="tab-five" class="tab-title">SU Elections</label>
+            <label for="tab-five" class="tab-title">SU</label>
             <div class="tab-content">
                 <!-- Blog posts with author Elections Show -->
                 <?php echo get_posts_by_author(10, 139); ?>
