de50742d1d5c975b59f3ffa7500b80bb1e28a73e
Harry Mumford-Turner <harrymt@me.com>
2015-09-16 13:51:31 +0100

    Added basic responsive styling to the homepage
    Reduced min-width of Audioboom channels, so they can fit onto smaller
    devices without breaking page layout.
    
    Added breathing room for all elements.
    
    Hidden the mini schedule on mobile, until we have designed a good
    responsive one.

--- home.php
+++ home.php
@@ -31,10 +31,10 @@
         <div class="row vertical">
             <h1>Headlines</h1>
             <div class="clip">
-                <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292950-students-with-disabilities-special-molly-o-brien-statement/embed/v3?eid=AQAAAApF0lUWPzIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
+                <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:250px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292950-students-with-disabilities-special-molly-o-brien-statement/embed/v3?eid=AQAAAApF0lUWPzIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
             </div>
             <div class="clip">
-                <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:300px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292768-students-complain-about-grad-ball/embed/v3?eid=AQAAAG9F0lVgPjIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
+                <iframe width="100%" height="150" style="background-color:transparent; display:block; min-width:250px; max-width:700px;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292768-students-complain-about-grad-ball/embed/v3?eid=AQAAAG9F0lVgPjIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
             </div>
             <h1>Schedule</h1>
             <div class="schedule mini-schedule">
