3bdf6c67b5de2b716f0f7ccde9968d398fd242e6
Harry Mumford-Turner <harrymt@me.com>
2015-10-24 16:51:37 +0100

    Updated header open graph link
    So social media sites use our specified image, rather than the first
    cover image on the home page

--- header.php
+++ header.php
@@ -6,7 +6,7 @@
     <title><?php echo get_page_title(); ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/cover_3.jpg">
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/cover_urn.jpg">
     <meta property="og:description" content="University Radio Nottingham is the multi-award–winning university radio station of the University of Nottingham Students’ Union. During term-time we broadcast locally on University Park Campus on 1350AM and worldwide via our website.">
     <!--[if gte IE 9]>
         <style type="text/css">
