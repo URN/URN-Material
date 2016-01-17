a48a18759e4fb59f42455158a6784856a4b74777
James Turner <me@jamesturner.im>
2016-01-17 11:20:18 +0000

    Add offline notice

--- header.php
+++ header.php
@@ -19,6 +19,9 @@
 </head>
 <body>
 <div id="page-wrap">
+    <div class="notice">
+        Unfortunately due to technical issues URN is currently offline. We apologise for the inconvenience and hope to be back up an running as soon as possible.
+    </div>
     <header id="header">
         <div class="wrapper">
             <a href="/" id="title">
@@ -27,6 +30,7 @@
             </a>
         </div>
     </header>
+
 
 <?php if ( has_nav_menu( 'header-menu' ) ) : ?>
 
