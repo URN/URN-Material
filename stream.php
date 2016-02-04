75811f5990f94069b218b277cc2f1c3a40bcfd70
James Turner <me@jamesturner.im>
2016-02-04 23:26:56 +0000

    Add <head> to stream

--- stream.php
+++ stream.php
@@ -5,9 +5,26 @@
 */
 show_admin_bar(false);
 remove_action('wp_head', '_admin_bar_bump_cb');
-wp_head();
 ?>
-
+<!doctype html>
+<html lang="en-GB">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?php echo get_page_title(); ?></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/images/cover_urn.jpg">
+    <meta property="og:description" content="University Radio Nottingham is the multi-award–winning university radio station of the University of Nottingham Students’ Union. During term-time we broadcast locally on University Park Campus on 1350AM and worldwide via our website.">
+    <!--[if gte IE 9]>
+        <style type="text/css">
+            .gradient {
+                filter: none;
+            }
+        </style>
+    <![endif]-->
+    <?php wp_head(); ?>
+</head>
 <body class="stream">
     <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="URN Logo">
     <h1>University Radio Nottingham</h1>
