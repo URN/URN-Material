d68c2668c5d63d040effaf1ef0b8da7b0356a2e7
Johnathan Graydon <johnathan99j@yahoo.com>
2016-04-25 11:11:49 +0100

    Added blog pagination

--- blogs.php
+++ blogs.php
@@ -5,6 +5,17 @@
 */
 get_header(); ?>
 
+<?php
+global $paged;
+$curpage = $paged ? $paged : 1;
+$args = array(
+    'orderby' => 'post_date',
+    'posts_per_page' => 5,
+    'post_status'      => 'publish',
+    'paged' => $paged
+);
+$query = new WP_Query($args);?>
+
 <div class="main-content">
 
     <div class="entry-content">
@@ -22,11 +33,7 @@ get_header(); ?>
     <div class="blog-wrapper">
         <?php
             // Get all the blogs
-            $posts = get_posts(array(
-              'post_type' => 'post',
-              'numberposts' => 100,
-              'orderby' => 'date'
-            ));
+            $posts = get_posts($args);
 
             echo "<ul class='blog-excerpt'>";
             foreach ( $posts as $post ) {
@@ -39,6 +46,28 @@ get_header(); ?>
         // End the loop.
         endwhile;
         ?>
+
+        <?php
+        //Pagination links
+        echo '
+        <div id="wp_pagination">
+          <a class="first_page" href="'.get_pagenum_link(1).'">&laquo;</a>
+          <a class="previous_page" href="'.get_pagenum_link(($curpage-1 > 0 ? $curpage-1 : 1)).'">&lsaquo;</a>';
+          for($i=$curpage - 5;$i<=$curpage + 5;$i++){
+            if ($i < 1) {//Stops displaying negative pages
+              $i = 0;
+            } elseif ($i > $query->max_num_pages) {//Stops displaying pages over the limit
+              break;
+            } else {
+              echo '<a class="'.($i == $curpage ? 'active ' : '').'page button" href="'.get_pagenum_link($i).'">'.$i.' </a>';
+            }
+          }
+          echo '
+          <a class="next_page" href="'.get_pagenum_link(($curpage+1 <= $query->max_num_pages ? $curpage+1 : $query->max_num_pages)).'">&rsaquo;</a>
+          <a class="last_page" href="'.get_pagenum_link($query->max_num_pages).'">&raquo;</a>
+        </div>';
+        ?>
+
     </div>
 </div> <!-- /.main-content -->
 <?php get_footer(); ?>
