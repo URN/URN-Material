6af6ffbc0b05ff12cd32fdc4b7f9c8feb2b6e87e
James Turner <me@jamesturner.im>
2016-05-07 13:33:20 +0100

    Tweak shows page to follow the rest of the site's styling

--- shows.php
+++ shows.php
@@ -34,39 +34,38 @@ get_header(); ?>
 
 <div class="main-content">
     <div class="entry-content">
-    <?php
-        // Start the loop.
-        while ( have_posts() ) : the_post();
+        <?php
+            // Start the loop.
+            while ( have_posts() ) : the_post();
 
-            // Include the page content template.
-            the_content();
+                // Include the page content template.
+                the_content();
 
-        // End the loop.
-        endwhile;
-        ?>
-    </div>
+            // End the loop.
+            endwhile;
+            ?>
 
-    <div class="shows">
-        <h1 id="on-air">ON-AIR shows</h1>
-        <p>This does not include online-shows</p>
-         <?php
-            // Print each show
-            $curr_cat = "";
-            foreach ($shows_output as $show_sorted) {
-                if ($curr_cat != $show_sorted['show_category']) {
-                    $cat = strtolower($show_sorted['show_category']);
-                    echo '<h1 id=' . str_replace(' ', '', $cat) . '>' . $show_sorted['show_category'] . '</h1>';
+        <div class="shows">
+            <h1 id="on-air">On-air shows</h1>
+            <?php
+                // Print each show
+                $curr_cat = "";
+                foreach ($shows_output as $show_sorted) {
+                    if ($curr_cat != $show_sorted['show_category']) {
+                        $cat = strtolower($show_sorted['show_category']);
+                        echo '<h2 id=' . str_replace(' ', '', $cat) . '>' . $show_sorted['show_category'] . '</h2>';
+                    }
+                    $curr_cat = $show_sorted['show_category'];
+                    echo '<div class="show">';
+                        echo '<h3><a href=' . get_term_link($show_sorted['show']) . '>' . $show_sorted['show']->name . '</a></h3>';
+                        echo '<p>' . $show_sorted['show']->description . '</p>';
+                    echo '</div>';
                 }
-                $curr_cat = $show_sorted['show_category'];
-                echo '<h2><a href=' . get_term_link($show_sorted['show']) . '>' . $show_sorted['show']->name . '</a></h2>';
-                echo '<p>' . $show_sorted['show']->description;
-                echo '<p>' . $show_sorted['name_prelude'];
-            }
-        ?>
+            ?>
 
-
-        <h1 id="off-air">OFF-AIR shows</h1>
-        <p>No shows found.</p>
+            <h1 id="off-air">Off-air shows</h1>
+            <p>No shows found.</p>
+        </div>
     </div>
 </div>
 
