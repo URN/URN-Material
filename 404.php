382855033b1c48aa91329cbb390568b4cde8cc70
James Turner <me@jamesturner.im>
2016-05-16 15:53:35 +0100

    Add correct class to main content container and remove inline styles

--- 404.php
+++ 404.php
@@ -1,5 +1,5 @@
 <?php get_header();?>
-<div class="main-content" style="text-align: center;">
+<div class="main-content entry-content">
     <h1>Page not found!</h1> <br />
     <p>Oops, it seems we couldn't find the page you were looking for.</p>
     <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
