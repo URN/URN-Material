83c0f1ae3a3b590d5af9802d2758fcb54b0d4ee5
James Turner <me@jamesturner.im>
2016-05-18 20:38:24 +0100

    Change default <p> size to 1em

--- podcasts.php
+++ podcasts.php
@@ -27,32 +27,32 @@ get_header(); ?>
             <button class="btn">See all Podcasts</button>
         </a>
 
-        <section class="tabs">
-            <input id="tab-podcasts" type="radio" name="grp" checked="checked"/>
-            <label for="tab-podcasts" class="tab-title">Podcasts</label>
-            <div class="tab-content">
-                <div class="audioboom-feed" data-childchannel="URN" data-channel-id="184841" data-channel-audioboom-type="users" data-channel-type="urn-podcasts" data-channel-name="URN Podcasts"></div>
-            </div>
-
-            <input id="tab-news-sports-culture" type="radio" name="grp" />
-            <label for="tab-news-sports-culture" class="tab-title">News, Sports &amp; Culture</label>
-            <div class="tab-content">
-                <div class="audioboom-feed" data-channel-id="4227797" data-channel-audioboom-type="users" data-channel-type="urn-speech" data-channel-name="URN Speech"></div>
-            </div>
-
-            <input id="tab-after-dark" type="radio" name="grp" />
-            <label for="tab-after-dark" class="tab-title">After Dark</label>
-            <div class="tab-content">
-                <div class="audioboom-feed" data-channel-id="4374438" data-channel-audioboom-type="users" data-channel-type="urn-after-dark" data-channel-name="URN AfterDark"></div>
-            </div>
-
-            <input id="tab-daytime" type="radio" name="grp" />
-            <label for="tab-daytime" class="tab-title">Daytime</label>
-            <div class="tab-content">
-                <div class="audioboom-feed" data-channel-id="3125607" data-channel-audioboom-type="users" data-channel-type="urn-daytime" data-channel-name="URN Daytime"></div>
-            </div>
-        </section>
     </div>
+    <section class="tabs">
+        <input id="tab-podcasts" type="radio" name="grp" checked="checked"/>
+        <label for="tab-podcasts" class="tab-title">Podcasts</label>
+        <div class="tab-content">
+            <div class="audioboom-feed" data-childchannel="URN" data-channel-id="184841" data-channel-audioboom-type="users" data-channel-type="urn-podcasts" data-channel-name="URN Podcasts"></div>
+        </div>
+
+        <input id="tab-news-sports-culture" type="radio" name="grp" />
+        <label for="tab-news-sports-culture" class="tab-title">News, Sports &amp; Culture</label>
+        <div class="tab-content">
+            <div class="audioboom-feed" data-channel-id="4227797" data-channel-audioboom-type="users" data-channel-type="urn-speech" data-channel-name="URN Speech"></div>
+        </div>
+
+        <input id="tab-after-dark" type="radio" name="grp" />
+        <label for="tab-after-dark" class="tab-title">After Dark</label>
+        <div class="tab-content">
+            <div class="audioboom-feed" data-channel-id="4374438" data-channel-audioboom-type="users" data-channel-type="urn-after-dark" data-channel-name="URN AfterDark"></div>
+        </div>
+
+        <input id="tab-daytime" type="radio" name="grp" />
+        <label for="tab-daytime" class="tab-title">Daytime</label>
+        <div class="tab-content">
+            <div class="audioboom-feed" data-channel-id="3125607" data-channel-audioboom-type="users" data-channel-type="urn-daytime" data-channel-name="URN Daytime"></div>
+        </div>
+    </section>
 </div>
 
 <?php get_footer(); ?>
