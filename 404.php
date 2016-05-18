30d68a543323bc91d68b2ee9e39cc2aac85bf9c4
James Turner <me@jamesturner.im>
2016-05-18 18:18:11 +0100

    Nest content in 'entry-content' div on 404 page

--- 404.php
+++ 404.php
@@ -1,7 +1,9 @@
 <?php get_header();?>
-<div class="main-content entry-content">
-    <h1>Page not found!</h1> <br />
-    <p>Oops, it seems we couldn't find the page you were looking for.</p>
-    <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
+<div class="main-content">
+    <div class="entry-content">
+        <h1>Page not found!</h1> <br />
+        <p>Oops, it seems we couldn't find the page you were looking for.</p>
+        <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
+    </div>
 </div>
 <?php get_footer(); ?>
