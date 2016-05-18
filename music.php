746d1d0c70f2e11971fb588ff5dd7794fcf4a1e8
James Turner <me@jamesturner.im>
2016-05-18 16:28:48 +0100

    Move tabbed content in 'your music' page into content container

--- music.php
+++ music.php
@@ -20,57 +20,56 @@ get_header(); ?>
             // End the loop.
             endwhile;
         ?>
-    </div>
 
-    <section class="tabs">
+        <section class="tabs">
+            <input id="tab-playlist-a" type="radio" name="grp" checked="checked"/>
+            <label for="tab-playlist-a" class="tab-title">Playlists</label>
+            <div class="tab-content">
+                <div class="videos">
+                    <!-- Youtube Grid of images -->
+                    <div class="youmax"></div>
 
-        <input id="tab-playlist-a" type="radio" name="grp" checked="checked"/>
-        <label for="tab-playlist-a" class="tab-title">Playlists</label>
-        <div class="tab-content">
-            <div class="videos">
-                <!-- Youtube Grid of images -->
-                <div class="youmax"></div>
-
-                <script>
-                    jQuery(document).ready(function( $ ) {
-                        // Settings (must be below youmax class)
-                        $('.youmax').youmax({
-                            apiKey: 'AIzaSyBWZBeWeXbWUHcNKbt-vEXH6q3ltP6CDLs', // harry.mumford-turner@urn1350.net
-                            youTubeChannelURL: "http://www.youtube.com/user/urn1350",
-                            youTubePlaylistURL: "PL9tY0BWXOZFvWi6WNdcokF_YvXUxyESRW",
-                            youmaxColumns: 3,
-                            showVideoInLightbox: false,
-                            maxResults: 20,
-                            showHeader: false,
-                            customTabs: [
-                                {
-                                    name: "Playlist A",
-                                    type: 'featured',
-                                    url: "PLj7bGB3G_znvbJ5E6hLDYn26akuU5HJkz"
-                                },
-                                {
-                                    name: "Playlist B",
-                                    type: 'featured',
-                                    url: "PLj7bGB3G_znu08f1pWCwCHOCpBAhRHC78"
-                                },
-                                {
-                                    name: "Playlist C",
-                                    type: 'featured',
-                                    url: "PLj7bGB3G_znschvxh-cXqPtX7DS54Ui5k"
-                                }
-                            ]
+                    <script>
+                        jQuery(document).ready(function( $ ) {
+                            // Settings (must be below youmax class)
+                            $('.youmax').youmax({
+                                apiKey: 'AIzaSyBWZBeWeXbWUHcNKbt-vEXH6q3ltP6CDLs', // harry.mumford-turner@urn1350.net
+                                youTubeChannelURL: "http://www.youtube.com/user/urn1350",
+                                youTubePlaylistURL: "PL9tY0BWXOZFvWi6WNdcokF_YvXUxyESRW",
+                                youmaxColumns: 3,
+                                showVideoInLightbox: false,
+                                maxResults: 20,
+                                showHeader: false,
+                                customTabs: [
+                                    {
+                                        name: "Playlist A",
+                                        type: 'featured',
+                                        url: "PLj7bGB3G_znvbJ5E6hLDYn26akuU5HJkz"
+                                    },
+                                    {
+                                        name: "Playlist B",
+                                        type: 'featured',
+                                        url: "PLj7bGB3G_znu08f1pWCwCHOCpBAhRHC78"
+                                    },
+                                    {
+                                        name: "Playlist C",
+                                        type: 'featured',
+                                        url: "PLj7bGB3G_znschvxh-cXqPtX7DS54Ui5k"
+                                    }
+                                ]
+                            });
                         });
-                    });
-                </script>
+                    </script>
+                </div>
             </div>
-        </div>
 
-        <input id="tab-interviews" type="radio" name="grp" />
-        <label for="tab-interviews" class="tab-title">Interviews</label>
-        <div class="tab-content">
-            <div class="audioboom-feed" data-channel-id="4374382" data-channel-audioboom-type="users" data-channel-type="urn-interviews" data-channel-name="URN Interviews"></div>
-        </div>
-    </section>
+            <input id="tab-interviews" type="radio" name="grp" />
+            <label for="tab-interviews" class="tab-title">Interviews</label>
+            <div class="tab-content">
+                <div class="audioboom-feed" data-channel-id="4374382" data-channel-audioboom-type="users" data-channel-type="urn-interviews" data-channel-name="URN Interviews"></div>
+            </div>
+        </section>
+    </div>
 </div>
 
 <?php get_footer(); ?>
