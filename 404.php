19c00bf7f788e71cab87b73f93f1c6121bf5a9a7
James Turner <me@jamesturner.im>
2016-07-08 21:01:05 +0100

    Only redirect to old site on 404 if old.urn returns '200 OK'

--- 404.php
+++ 404.php
@@ -2,7 +2,7 @@
 $requestPath = $_SERVER['REQUEST_URI'];
 $oldLink = 'http://old.urn1350.net' . $requestPath;
 $file_headers = @get_headers($oldLink);
-if ($file_headers[0] !== 'HTTP/1.1 404 Not Found') {
+if ($file_headers[0] == 'HTTP/1.1 200 OK') {
     header('Location: ' . $oldLink);
     die();
 }
