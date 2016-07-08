a33520c5ddd1b3f4cbd9e78825a23bb0de7b23a7
James Turner <me@jamesturner.im>
2016-07-08 21:03:07 +0100

    Add correct html structure back into 404.php

--- 404.php
+++ 404.php
@@ -9,8 +9,10 @@ if ($file_headers[0] == 'HTTP/1.1 200 OK') {
 get_header();
 ?>
 <div class="main-content">
-    <h1>Page not found!</h1>
-    <p>Sorry we couldn't find the page you were looking for on this site (or the old one!)</p>
-    <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
+    <div class="entry-content">
+        <h1>Page not found!</h1> <br />
+        <p>Oops, it seems we couldn't find the page you were looking for.</p>
+        <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
+    </div>
 </div>
 <?php get_footer(); ?>
