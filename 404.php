e38c59c4e155abc082403c8ebb4be1356f102b4b
James Turner <me@jamesturner.im>
2016-07-08 20:36:49 +0100

    Revert "Updated 404 to not search old.urn1350"
    This reverts commit 66f92cea327bcb47944f746c8eadfadfe475c48b.

--- 404.php
+++ 404.php
@@ -1,9 +1,16 @@
-<?php get_header();?>
+<?php
+$requestPath = $_SERVER['REQUEST_URI'];
+$oldLink = 'http://old.urn1350.net' . $requestPath;
+$file_headers = @get_headers($oldLink);
+if ($file_headers[0] !== 'HTTP/1.1 404 Not Found') {
+    header('Location: ' . $oldLink);
+    die();
+}
+get_header();
+?>
 <div class="main-content">
-    <div class="entry-content">
-        <h1>Page not found!</h1> <br />
-        <p>Oops, it seems we couldn't find the page you were looking for.</p>
-        <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
-    </div>
+    <h1>Page not found!</h1>
+    <p>Sorry we couldn't find the page you were looking for on this site (or the old one!)</p>
+    <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
 </div>
 <?php get_footer(); ?>
