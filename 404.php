98070a98cfd0e12cf75cbf262470d6ade61b4660
Johnathan Graydon <johnathan99j@yahoo.com>
2016-07-15 09:36:02 -0700

    Small tweak to 404 wording.
    Added back in bit about checking the old site.

--- 404.php
+++ 404.php
@@ -11,7 +11,7 @@ get_header();
 <div class="main-content">
     <div class="entry-content">
         <h1>Page not found!</h1> <br />
-        <p>Oops, it seems we couldn't find the page you were looking for.</p>
+        <p>Sorry we couldn't find the page you were looking for on this site (or the old one!)</p>
         <p>Try going back to the <a href="/">homepage</a> and navigate to the page you were looking for from there.</p>
     </div>
 </div>
