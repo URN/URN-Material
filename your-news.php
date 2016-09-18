ddeb443c7a89addbe72691ef437998ba14ac99b9
Johnathan Graydon <johnathan99j@yahoo.com>
2016-09-18 14:16:51 +0100

    Redesigned Your News per request of new committee
    Cut down the number of tabs and moved the headlines to the right hand side.
    Added news posts on the left, currently only shows categories "SU" and
    "Pulse".

--- your-news.php
+++ your-news.php
@@ -7,19 +7,35 @@ get_header(); ?>
 
 <div class="main-content">
 
-    <!-- Todays Headlines as 2 latest Audioboom embeds -->
-    <div class="audioboom-headlines entry-content">
-        <h1>Latest Headlines</h1>
-       <!--  <iframe width="100%" height="150" style="background-color:transparent;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292950-students-with-disabilities-special-molly-o-brien-statement/embed/v3?eid=AQAAALz-9VUWPzIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
-        <iframe width="100%" height="150" style="background-color:transparent;" frameborder="0" allowtransparency="allowtransparency" scrolling="no" src="//embeds.audioboom.com/boos/3292768-students-complain-about-grad-ball/embed/v3?eid=AQAAAAKG8VVgPjIA&amp;player_theme=light&amp;link_color=%235e44cb&amp;image_option=small&amp;show_title=true" title="audioBoom player"></iframe>
- -->
-        <div class="audioboom-feed" data-card-type="0" data-channel-id="4227841" data-channel-audioboom-type="users" data-channel-type="urn-headlines" data-channel-name="URN Headlines"></div>
+    <div class="row row-wide">
+        <div class="row vertical">
+            <h1>News Posts</h1>
+            <?php
+            // Get the most recent blog
+            $posts = get_posts(array(
+                'numberposts' => 3,
+                'category_name' => 'Pulse, SU'
+            ));
 
+            echo "<ul class='blog-excerpt'>";
+            foreach ( $posts as $post ) {
+                echo format_blog_excerpt($post, false, h2);
+            }
+            echo "</ul>";
+            ?>
 
-        <div class="btn-wrapper">
-            <a href="//audioboom.com/URNHeadlines">
-                <button class="btn">See all headlines</button>
-            </a>
+        </div>
+
+        <div class="row vertical">
+            <h1>Latest Headlines</h1>
+            <!-- Todays Headlines as 2 latest Audioboom embeds -->
+            <div class="audioboom-feed" data-card-type="0" data-channel-id="4227841" data-channel-audioboom-type="users" data-channel-type="urn-headlines" data-channel-name="URN Headlines" data-card-type="0" data-show-image="false"></div>
+
+            <div class="btn-wrapper">
+                <a href="//audioboom.com/URNHeadlines">
+                    <button class="btn">See all headlines</button>
+                </a>
+            </div>
         </div>
     </div>
 
@@ -27,45 +43,37 @@ get_header(); ?>
         <input id="tab-one" type="radio" name="grp" checked="checked"/>
         <label for="tab-one" class="tab-title">Pulse</label>
         <div class="tab-content">
-            <div class="audioboom-feed" data-channel-id="3119406" data-channel-type="urn-pulse" data-channel-audioboom-type="channels" data-channel-name="URN Pulse"></div>
-        </div>
-
-        <input id="tab-three" type="radio" name="grp" />
-        <label for="tab-three" class="tab-title">The Column</label>
-        <div class="tab-content">
-            <!-- Blog posts with author URN -->
-            <?php echo get_posts_by_author(10, 2); ?>
-        </div>
-
-        <input id="tab-four" type="radio" name="grp" />
-        <label for="tab-four" class="tab-title">Uni of Sport</label>
-        <div class="tab-content">
-            <!-- Blog posts with category Sport -->
+            <!-- Blog posts with category Pulse -->
             <?php
-                // Get the most recent blog
-                $posts = get_posts(array('category_name' => 'Sport'));
-
-                echo "<ul class='blog-excerpt'>";
-                foreach ( $posts as $post ) {
-                    echo format_blog_excerpt($post, false);
-                }
-                echo "</ul>";
-                ?>
+                // // Get the most recent pulse blog
+                // $posts = get_posts(array('category_name' => 'Pulse','posts_per_page' => 8));
+                //
+                // echo "<ul class='blog-excerpt'>";
+                // foreach ( $posts as $post ) {
+                //     setup_postdata( $post );
+                //     echo format_blog_excerpt($post, true, h2);
+                // }
+                // echo "</ul>";
+                echo do_shortcode('[ajax_load_more post_type="post" category="pulse" max_pages="0" posts_per_page="8"]');
+            ?>
+            <!-- <div class="audioboom-feed" data-channel-id="3119406" data-channel-type="urn-pulse" data-channel-audioboom-type="channels" data-channel-name="URN Pulse"></div> -->
         </div>
 
-        <input id="tab-five" type="radio" name="grp" />
-        <label for="tab-five" class="tab-title">SU</label>
+        <input id="tab-two" type="radio" name="grp" />
+        <label for="tab-two" class="tab-title">SU-News</label>
         <div class="tab-content">
             <!-- Blog posts with category SU -->
             <?php
-                // Get the most recent blog
-                $posts = get_posts(array('category_name' => 'SU'));
-
-                echo "<ul class='blog-excerpt'>";
-                foreach ( $posts as $post ) {
-                    echo format_blog_excerpt($post, false);
-                }
-                echo "</ul>";
+                // // Get the most recent SU-News blog
+                // $posts = get_posts(array('category_name' => 'SU','posts_per_page' => 8));
+                //
+                // echo "<ul class='blog-excerpt'>";
+                // foreach ( $posts as $post ) {
+                //     setup_postdata( $post );
+                //     echo format_blog_excerpt($post, true, h2);
+                // }
+                // echo "</ul>";
+                echo do_shortcode('[ajax_load_more post_type="post" category="SU" max_pages="0" posts_per_page="8"]');
             ?>
         </div>
     </section>
