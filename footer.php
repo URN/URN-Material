197d48356061e65e39cb26bc413fb228cb05ddf5
Ricky Eatough <55627793+rickyeatough@users.noreply.github.com>
2020-11-12 02:29:35 +0000

    Added and updated get in touch details in footer
    Added link for branding pack, as well as for our Instagram and updated authors.

--- footer.php
+++ footer.php
@@ -11,14 +11,16 @@
         <div>
             <strong>Winner of Best Student Radio Station at the Student Radio Awards 2010, 2011, 2012, 2013, 2014, 2016,
                 2017 and 2018!</strong><br>
-            Contact the on-air presenters by sending us an online message, a text on <strong>07903 545 252</strong> or
+            Contact the on-air presenters by messaging us on Instagram <a href="https://instagram.com/urn1350" target="_blank" rel="noopener">@URN1350</a>, or
             tweet <a href="https://twitter.com/urn1350" target="_blank" rel="noopener">@URN1350</a><br>
             Find out more about us, and get in touch on our <a
-                    href="<?php echo get_permalink( get_page_by_path( 'about us' ) ); ?>">about</a> page<br>
+                    href="<?php echo get_permalink( get_page_by_path( 'about us' ) ); ?>">about</a> page. 
+			<a href="<?php echo get_permalink( get_page_by_path( 'branding pack' ) ); ?>">Branding Pack</a> <br>
             Web Design &amp; Development by <strong><a href="https://jamesturner.im" target="_blank" rel="noopener">James
                     Turner</a></strong>, <strong><a href="https://harrymt.com" target="_blank" rel="noopener">Harry
-                    Mumford-Turner</a></strong> and <strong><a href="https://github.com/johnathan99j" target="_blank"
-                                                               rel="noopener">Johnathan Graydon</a></strong>
+                    Mumford-Turner</a></strong>,<strong><a href="https://github.com/johnathan99j" target="_blank"
+                    rel="noopener">Johnathan Graydon</a></strong>and<strong><a 
+		    href="https://www.linkedin.com/in/rickyeatough/" target="_blank" rel="noopener">Ricky Eatough</a></strong>
         </div>
     </div>
 </footer>
